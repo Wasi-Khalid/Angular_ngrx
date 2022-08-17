97238a61a5e838911149989994d5d5e44cd147c7
Wasi_Khalid <aman.wasi01@gmail.com>
2022-08-17 22:56:10 +0500

    first commit

--- src/app/features/layout/common/header/header.component.ts
+++ src/app/features/layout/common/header/header.component.ts
@@ -4,6 +4,7 @@ import { Observable, Subscription } from 'rxjs';
 import { LoadCurrency } from 'src/app/store/actions/currency.action';
 import * as fromSelector from 'src/app/store/selectors/currency.selector';
 import { Currency } from 'src/app/store/models/currency.model';
+import {BreakpointObserver} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-header',
@@ -15,6 +16,7 @@ export class HeaderComponent implements OnInit {
   data: Subscription | any;
   currency$: Observable<Currency[]>;
   result: any;
+  // temp: BreakpointObserver;
   constructor(
     private store: Store<Currency[]>
   ) {
